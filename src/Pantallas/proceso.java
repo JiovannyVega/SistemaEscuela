3eac7df67ad4ab486427e4a22cf2efbb583b76ab
Rodrig0304 <vega.morales.21027@itsmante.edu.mx>
2023-05-04 13:09:32 -0600

    se limpio el codigo

--- src/Pantallas/proceso.java
+++ src/Pantallas/proceso.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Pantallas;
-
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Rodrigo
- */
 public class proceso extends javax.swing.JFrame {
 
-    /**
-     * Creates new form proceso
-     */
     public proceso() {
         initComponents();
         setUndecorated(true);
